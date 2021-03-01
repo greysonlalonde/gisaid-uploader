3f016aefbe6b018fc0f268653a427ad02788ccd1
greysonlalonde <lalondeg1@michigan.gov>
2021-03-01 09:24:08 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -4,32 +4,37 @@ import pandas as pd
 from Bio import SeqIO
 
 
-
 def split_every(n, iterable):
     iterable = iter(iterable)
     yield from iter(lambda: list(islice(iterable, n)), [])
 
 
-def read_files(args):
-    options = [".json", ".csv", ".fasta", ".fna", ".ffn", ".faa", ".frn"]
- 
-    for x in args:
-        if x.lower().endswith(options[0]):
-            with open(x, "r") as authfile:
-                authf = json.loads(authfile.read())
-
-
-        elif x.lower().endswith((options[2])):
-            seq = {k.id: str(k.seq) for k in SeqIO.parse(x, "fasta")}
-
-
-        elif x.lower().endswith(options[1]):
-            metadata = pd.read_csv(x).apply(lambda x: x.to_dict(), axis=1)
-            {i.update({"covv_sequence": seq[k] for k in i.values() if k in seq}) for i in metadata}
-
+def check_file(fname):
+    d = {}
+    for i in fname:
+        if re.search("\.csv$", i, flags=re.IGNORECASE):
+            d["csv"] = i
+        elif re.search("\.json$", i, flags=re.IGNORECASE):
+            d["cred"] = i
+        elif re.search("\.fa$", i, flags=re.IGNORECASE):
+            d["fa"] = i
         else:
-            pass
+            print("Invalid file")
+    return d
 
-        
-    d = [split_every(500, metadata), authf]                             
+
+def read_files(args):
+    data = check_file(args)
+
+    with open(data["cred"], "r") as authfile:
+        authf = json.loads(authfile.read())
+    seq = {k.id: str(k.seq) for k in SeqIO.parse(data["fa"], "fasta")}
+
+    metadata = pd.read_csv(data["csv"]).apply(lambda x: x.to_dict(), axis=1)
+    {
+        i.update({"covv_sequence": seq[k] for k in i.values() if k in seq})
+        for i in metadata
+    }
+
+    d = [split_every(500, metadata), authf]
     return d
