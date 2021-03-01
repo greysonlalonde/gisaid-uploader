de6ba179af206f1768067ee2b00b11911b6924c7
greysonlalonde <lalondeg1@michigan.gov>
2021-03-01 10:00:08 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -2,6 +2,7 @@ from itertools import islice
 import json, requests, time
 import pandas as pd
 from Bio import SeqIO
+import re
 
 
 def split_every(n, iterable):
@@ -11,7 +12,7 @@ def split_every(n, iterable):
 
 def check_file(fname):
     d = {}
-    for i in fname:
+    for i in fname[0]:
         if re.search("\.csv$", i, flags=re.IGNORECASE):
             d["csv"] = i
         elif re.search("\.json$", i, flags=re.IGNORECASE):
@@ -19,22 +20,27 @@ def check_file(fname):
         elif re.search("\.fa$", i, flags=re.IGNORECASE):
             d["fa"] = i
         else:
-            print("Invalid file")
+            d['error'] = i
     return d
 
 
 def read_files(args):
     data = check_file(args)
+    try:
+        with open(data["cred"], "r") as authfile:
+            authf = json.loads(authfile.read())
+        seq = {k.id: str(k.seq) for k in SeqIO.parse(data["fa"], "fasta")}
 
-    with open(data["cred"], "r") as authfile:
-        authf = json.loads(authfile.read())
-    seq = {k.id: str(k.seq) for k in SeqIO.parse(data["fa"], "fasta")}
+        metadata = pd.read_csv(data["csv"]).apply(lambda x: x.to_dict(), axis=1)
+        {
+            i.update({"covv_sequence": seq[k] for k in i.values() if k in seq})
+            for i in metadata
+        }
+        
+        d = [split_every(500, metadata), authf]
+    except KeyError:
+        d = print(f'{data["error"]} not found')
+    
 
-    metadata = pd.read_csv(data["csv"]).apply(lambda x: x.to_dict(), axis=1)
-    {
-        i.update({"covv_sequence": seq[k] for k in i.values() if k in seq})
-        for i in metadata
-    }
-
-    d = [split_every(500, metadata), authf]
+    
     return d
