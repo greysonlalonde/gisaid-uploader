28a3332920bd56f931249a21a58cd107a553681c
greysonlalonde <lalondeg1@michigan.gov>
2021-03-02 15:56:46 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 from itertools import islice
 import json, requests, time
+from configparser import ConfigParser
 import pandas as pd
 from Bio import SeqIO
 import re
@@ -10,13 +11,28 @@ config.read('config.py')
 auth_path = config.get('FILES', 'AUTH_FILE')
 
 
-def split_every(n, iterable):
-    iterable = iter(iterable)
-    yield from iter(lambda: list(islice(iterable, n)), [])
+def authfile():
+    with open(auth_path, "r") as authfile:
+        authf = json.loads(authfile.read())
+    return authf
 
 
+
+def logfile(*args):
+    if args[1]['rc'] != 'ok':
+        with open('logfile.txt', 'a+') as f:
+            line = str('\n'+ f'{args[0]}: {args[1]["validation"]}')
+            f.write(line)
+    else:
+        pass
+    
+    
 def check_file(fname):
     d = {}
+    if 'collated' in fname[0]:
+        d.update(collated=True)
+    else:
+        d.update(collated=False)
     try:
         for i in fname[0]:
             if re.search("\.csv$", i, flags=re.IGNORECASE):
@@ -24,7 +40,7 @@ def check_file(fname):
             elif re.search("\.fa$", i, flags=re.IGNORECASE):
                 d["fa"] = i
             else:
-                d['error'] = i
+                pass
     except IndexError:
         d = print("File Error")
     return d
@@ -32,24 +48,41 @@ def check_file(fname):
 
 def read_files(args):
     data = check_file(args)
-    try:
-        with open(auth_path, "r") as authfile:
-            authf = json.loads(authfile.read())
-        seq = {k.id: str(k.seq) for k in SeqIO.parse(data["fa"], "fasta")}
+    print(data)
+    if data['collated'] == True:
 
-        metadata = pd.read_csv(data["csv"]).apply(lambda x: x.to_dict(), axis=1)
-        {
-            i.update({"covv_sequence": seq[k] for k in i.values() if k in seq})
-            for i in metadata
-        }
+        x = (pd.read_csv(data['csv'], index_col=0))
+        x.rename({'CollectionDate':'covv_collection_date'},axis=1,inplace=True)
+        x['covv_collection_date'] = pd.to_datetime(x['covv_collection_date'])
+        x['covv_collection_date'] = x['covv_collection_date'].astype(str)
+        x = (x.fillna('')).replace("NaT", '')
+        d = x.apply(lambda x: x.to_dict(), axis=1)
         
-        d = [split_every(500, metadata), authf]
-    except KeyError:
-        d = print(f'{data["error"]} not found')
-    
-    except TypeError:
-        d = print('file not found')
-    
+        
+    else:
+        try:
+            seq = {k.id: str(k.seq) for k in SeqIO.parse(data["fa"], "fasta")}
+
+
+            df = pd.read_csv(data["csv"])
+            df['covv_collection_date'] = pd.to_datetime(df['covv_collection_date'])
+            df['covv_collection_date'] = df['covv_collection_date'].astype(str)
+
+
+            metadata = df.apply(lambda x: x.to_dict(), axis=1)
+            {
+                i.update({"covv_sequence": seq[k] for k in i.values() if k in seq})
+                for i in metadata
+            }
+            d = metadata
+
+
+        except KeyError:
+            d = print(f'{data["error"]} not found')
+
+        except TypeError:
+            d = print('file not found')
+
 
     
     return d
