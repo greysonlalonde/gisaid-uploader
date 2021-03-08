40be98cf6bc05ecefad130675573f80bd09119f2
greysonlalonde <lalondeg1@michigan.gov>
2021-03-08 12:06:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
-    name="gisaid-uploader",
+    name="gisaid",
     url="https://github.com/greysonlalonde/gisaid-uploader",
     download_url="https://github.com/greysonlalonde/gisaid-uploader/archive/v1.0.2-beta.tar.gz",
     author="Greyson R. LaLonde",
@@ -10,4 +16,7 @@ setup(
     install_requires=["pandas", "numpy", "biopython", "requests"],
     version="v1.0.1-beta",
     license="MIT",
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+
 )
