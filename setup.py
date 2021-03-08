4d77aa71ce144875c5cbc39eefa0576e94ace13f
greysonlalonde <lalondeg1@michigan.gov>
2021-03-08 10:12:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(
     name='gisaid',
-    url='https://github.com/greysonlalonde/gisaid-uploader/archive/v1.0.1-beta.tar.gz',
+    url='https://github.com/greysonlalonde/gisaid-uploader',
+    download_url = 'https://github.com/greysonlalonde/gisaid-uploader/archive/v1.0.1-beta.tar.gz',
     author='Greyson R. LaLonde',
     author_email='greyson.r.lalonde@wmich.edu',
     packages=['gisaid'],
